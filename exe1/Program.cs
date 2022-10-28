a3ee9307935c4604779d0d9a8c44237b37338708
sofa <sv26362@yandex.ru>
2022-10-28 15:17:08 +0700

    well

---
+++ exe1/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("enter numbers separeted by spase ");
+int [] userarray = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+
+int count = 0;
+int positive(int[] userarray)
+{
+  for (int i = 0; i < userarray.Length; i++)
+  {
+      if (userarray[i] >= 0)
+      {
+          count++;
+      }
+  }
+  return count;
+}
+
+Console.WriteLine();
+Console.WriteLine($"numbers > 0: " + (positive(userarray)));
---
+++ exe2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("enter b1 = ");
+double b1 = double.Parse(Console.ReadLine());
+Console.WriteLine("enter k1 = ");
+double k1 = double.Parse(Console.ReadLine());
+Console.WriteLine("enter b2 = ");
+double b2 = double.Parse(Console.ReadLine());
+Console.WriteLine("enter k2 = ");
+double k2 = double.Parse(Console.ReadLine());
+
+double x = (b2 - b1) / (k1 - k2);
+double y = k1 * x + b1;
+
+
+Console.WriteLine($"intersection point X: {x}, Y: {y}");
